b117b0c549a62ea890bf835c7a0f3c500063ef88
Adil Khan <adiliitk2k16@gmal.com>
2023-08-20 22:25:12 +0530

    Kth bit status

--- BitManuplation/KthSetBit.cpp
+++ BitManuplation/KthSetBit.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+//Method 1 power method or division method
+bool kthsetbit(int num, int k)
+{
+    int setbit= pow(2,k-1);
+    return num&setbit;
+}
+
+//Method 2 using shift oerator (right or left)
+bool kthSetbit(int num, int k)
+{
+    int Setbit=(1<<(k-1));
+
+    return num&Setbit;
+}
 
 int main()
 {
+    int num =7; 
+    int k2=2, k5=5;
 
+    cout<<kthsetbit(num, k2)<<endl;
+
+    cout<<kthSetbit(num, k2)<<endl;
+
+    cout<<kthsetbit(num, k5)<<endl;
+
+    cout<<kthSetbit(num, k5)<<endl;
+    
 }
