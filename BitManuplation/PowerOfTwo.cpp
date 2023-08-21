af11862d6016178a494ec836567978a687ea68ea
Adil Khan <adiliitk2k16@gmal.com>
2023-08-21 22:21:18 +0530

    Power of two

--- BitManuplation/PowerOfTwo.cpp
+++ BitManuplation/PowerOfTwo.cpp
@@ -2,7 +2,26 @@
 #include<math.h>
 using namespace std;
 
+//Method 1 using log
+
+bool powerofTwo(unsigned int num)
+{
+    if (num==0) return false;
+    return (ceil(log2(num))==floor(log2(num)));
+}
+
+//finding power of two using brian kernighan's algorithms
+bool poweroftwo(unsigned int num)
+{
+    return (num!=0)&&((num&(num-1))==0);
+}
+
 int main()
 {
+    unsigned int num1 = 30, num2=128;
+    cout<<poweroftwo(num1)<<endl;
+    cout<<poweroftwo(num2)<<endl;
 
+    cout<<powerofTwo(num1)<<endl;
+    cout<<powerofTwo(num2)<<endl;
 }
