8972c6f1722957aacb3f3d3c9bd34051d0aea63a
Adil Khan <adiliitk2k16@gmal.com>
2023-08-21 22:05:40 +0530

    No of set bits

--- BitManuplation/CountSetBit.cpp
+++ BitManuplation/CountSetBit.cpp
@@ -2,7 +2,37 @@
 #include<math.h>
 using namespace std;
 
+//Method 1 of finding no of set bit
+unsigned int countSetbits(unsigned int num)
+{
+    unsigned int ans=0;
+    while(num)
+    {
+        ans+=num&1;
+        num>>=1;
+    }
+    return ans;
+}
+
+unsigned int countSetBits(unsigned int num)
+{
+    unsigned int ans=0;
+
+    while(num)
+    {
+        num&=(num-1);
+
+        ans++;
+    }
+
+    return ans;
+}
+
 int main()
 {
-    cout<<"Hello World";
+    unsigned int num=9;
+    cout<<countSetbits(num)<<endl;
+
+    cout<<countSetBits(num)<<endl;
+
 }
