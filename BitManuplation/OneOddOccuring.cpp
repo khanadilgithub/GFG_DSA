d6b97172c916e717d1ad976b1c5a8425fdbbafba
Adil Khan <adiliitk2k16@gmal.com>
2023-08-21 22:41:01 +0530

    odd ocuuring number

--- BitManuplation/OneOddOccuring.cpp
+++ BitManuplation/OneOddOccuring.cpp
@@ -2,7 +2,21 @@
 #include<math.h>
 using namespace std;
 
+int oddoccuringnum(int arr[], int n)
+{
+    int ans =0;
+
+    for (int i=0; i<n; i++)
+    {
+        ans=ans^arr[i];
+    }
+    return ans;
+}
+
 int main()
 {
+    int arr[] ={2,3,5,4,5,2,4,3,5,2,4,4,2};
+    int n=13;
 
+    cout<<oddoccuringnum(arr, n);
 }
