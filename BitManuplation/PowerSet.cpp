820b60c4b2ce479301d6451415d1f88f9954aaea
Adil Khan <adiliitk2k16@gmal.com>
2023-08-28 15:11:09 +0530

    Power set

--- BitManuplation/PowerSet.cpp
+++ BitManuplation/PowerSet.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
+#include<string>
 #include<math.h>
 using namespace std;
 
+void powerset(string str)
+{
+    int n = str.size();
+    int p = (1<<n);
+    cout<<p<<endl;
+
+    for(int i=0; i<p; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i&(1<<j))
+            {
+                cout<<str[j];
+            }
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
+    string str = "abc";
+
+    powerset(str);
     
 }
