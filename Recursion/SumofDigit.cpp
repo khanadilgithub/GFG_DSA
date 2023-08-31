cbd9cfd2af0487f4a81ed1e8679d25a4d79c6b9c
Adil Khan <adiliitk2k16@gmal.com>
2023-08-31 22:59:39 +0530

    Sum of Digit using rcursion

---
+++ Recursion/SumofDigit.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int SumOfDigits(int num)
+{
+    if(num==0) return 0;
+
+    return num%10+SumOfDigits(num/10);
+}
+
+int main()
+{
+    int num = 121;
+    int num1 = 253;
+    int num2 = 9987;
+
+    cout<<SumOfDigits(num)<<endl;
+
+    cout<<SumOfDigits(num1)<<endl;
+
+    cout<<SumOfDigits(num2)<<endl;
+}
