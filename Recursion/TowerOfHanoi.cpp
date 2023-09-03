a3a547dc47ee4398131fb301df50907d5d4c4f2c
Adil Khan <adiliitk2k16@gmal.com>
2023-09-03 13:12:24 +0530

    Tower of Hanoi

--- Recursion/TowerOfHanoi.cpp
+++ Recursion/TowerOfHanoi.cpp
@@ -2,7 +2,21 @@
 #include<math.h>
 using namespace std;
 
+void TOH(int numofdisc, char A, char B, char C)
+{
+    if(numofdisc==1)
+    {
+        cout<<"Move 1 from "<<A<<" to "<<C<<endl;
+        return;
+    }
+    TOH(numofdisc-1, A, C, B);
+    cout<<"Move "<<numofdisc<<" form "<<A<<" to "<<C<<endl;
+    TOH(numofdisc-1, B, A, C);
+
+}
+
 int main()
 {
-
+    int numofdisc=5;
+    TOH(numofdisc, 'A','B','C');
 }
