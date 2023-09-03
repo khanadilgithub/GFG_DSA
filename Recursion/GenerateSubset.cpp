d835dd0c637769326aba62426fff15f22ff8c91e
Adil Khan <adiliitk2k16@gmal.com>
2023-09-03 11:15:45 +0530

    Subset Using recursion

--- Recursion/GenerateSubset.cpp
+++ Recursion/GenerateSubset.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 #include<math.h>
+#include<string>
 using namespace std;
+
+void Subset(string& str, string substr ="", int indx =0)
+{
+    if(indx==str.length())
+    {
+        cout<<substr <<" ";
+        return;
+    }
+
+    Subset(str, substr, indx+1);
+    Subset(str, substr+str[indx], indx+1);
+}
 
 int main()
 {
+    string str = "ABC";
 
+    Subset(str);
 }
