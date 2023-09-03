1889c2decccf989d5e7ca76fccf106696594beb4
Adil Khan <adiliitk2k16@gmal.com>
2023-09-03 16:10:17 +0530

    Josphus Problem

--- Recursion/Josephus.cpp
+++ Recursion/Josephus.cpp
@@ -2,7 +2,17 @@
 #include<math.h>
 using namespace std;
 
+int josphus(int num, int k)
+{
+    if(num==1) return 0;
+
+    return (josphus(num-1,k)+k)%num;
+}
+
 int main()
 {
+    int num=5;
+    int k=3;
 
+    cout<<josphus(num,k)<<endl;
 }
