dc8598713806bc7490e4f0ac5c5f7b722a73f793
Adil Khan <adiliitk2k16@gmal.com>
2023-09-09 22:42:25 +0530

    Permution non working code

--- Recursion/Permuation.cpp
+++ Recursion/Permuation.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 #include<math.h>
+#include<string>
 using namespace std;
+
+void permutation(string str, int i=0)
+{
+    if(i==(str.size()-1)) 
+    {
+        cout<<str<<endl;
+        return;
+    }
+
+    for(int j=i; i<str.size(); j++)
+    {
+        swap(str[i],str[j]);
+
+        permutation(str, i+1);
+
+        swap(str[i],str[j]);
+    }
+}
 
 int main()
 {
+    string str ="ABC";
 
+    permutation(str);
 }
