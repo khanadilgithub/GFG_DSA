53c88955d567be3b7671b9dd915dca8d062d84d4
Adil Khan <adiliitk2k16@gmal.com>
2023-09-10 14:57:28 +0530

    Sorted array

--- Arrays/SortedArray.cpp
+++ Arrays/SortedArray.cpp
@@ -2,7 +2,34 @@
 #include<math.h>
 using namespace std;
 
+bool IsSorted(int arr[], int n)
+{
+    for (int i=1; i<n; i++)
+    {
+        if(arr[0]<arr[n-1])
+        {
+            if(arr[i]<arr[i-1]) return false; //Incresing order sorted 
+        }
+        else
+        {
+            if(arr[i]>arr[i-1]) return false; //Decreasing order sorted
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
+    int arr[] = {8,10,12,15};
 
+    int arr1[] = {8,12,10,11};
+
+    int arr2[] = {100, 50, 20, 10};
+
+    cout<<IsSorted(arr,4)<<endl;
+
+    cout<<IsSorted(arr1,4)<<endl;
+
+    cout<<IsSorted(arr2,4)<<endl;
 }
