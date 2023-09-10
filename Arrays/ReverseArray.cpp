222b5a3c90d05b50ff7e8075534f62862532da12
Adil Khan <adiliitk2k16@gmal.com>
2023-09-10 15:08:05 +0530

    Reverse array

--- Arrays/ReverseArray.cpp
+++ Arrays/ReverseArray.cpp
@@ -2,7 +2,29 @@
 #include<math.h>
 using namespace std;
 
+void Reverse(int arr[], int n)
+{
+    int lftindx=0, rgtindx=n-1;
+
+    while(lftindx<rgtindx)
+    {
+        swap(arr[lftindx], arr[rgtindx]);
+        lftindx++;
+        rgtindx--;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+}
+
+
+
 int main()
 {
-
+    int arr[] = {0,1,2,3,4,5,6,7,8,9};
+    Reverse(arr, 10);
 }
