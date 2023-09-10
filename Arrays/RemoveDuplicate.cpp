5cda3a6407dfa3577f08e3d65341ef2c956eb150
Adil Khan <adiliitk2k16@gmal.com>
2023-09-10 22:55:46 +0530

    Remove duplicate

--- Arrays/RemoveDuplicate.cpp
+++ Arrays/RemoveDuplicate.cpp
@@ -2,7 +2,25 @@
 #include<math.h>
 using namespace std;
 
+int removeDuplicates(int arr[], int n)
+{
+    int res =1;
+
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i]!=arr[res-1])
+        {
+            arr[res]=arr[i];
+            res++;
+        }
+    }
+
+    return res;
+}
+
 int main()
 {
+    int arr[] ={10,20,20,30,30,30,30};
 
+    cout<<removeDuplicates(arr, 7)<<endl;
 }
