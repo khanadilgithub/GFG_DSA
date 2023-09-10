ea58c2878023d29d936c84718d1523c9915a159e
Adil Khan <adiliitk2k16@gmal.com>
2023-09-10 23:10:27 +0530

    Move Zeros to end

--- Arrays/MoveZero.cpp
+++ Arrays/MoveZero.cpp
@@ -2,7 +2,28 @@
 #include<math.h>
 using namespace std;
 
+void moveZeros(int arr[], int n)
+{
+    int count=0;
+
+    for (int i=0; i<n; i++)
+    {
+        if(arr[i]!=0)
+        {
+            swap(arr[i],arr[count]);
+            count++;
+        }
+    }
+
+    for (int j=0; j<n; j++) cout<<arr[j]<<" ";
+
+    cout<<endl;
+}
+
 int main()
 {
+    int arr[] = {10,20,0,0,0,30,50,70,0,0,0,0,90};
 
+    moveZeros(arr, 13);
 }
+
