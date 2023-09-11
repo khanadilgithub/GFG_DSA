5dd864b9658340c4abd46d9dea430e94f0c54374
Adil Khan <adiliitk2k16@gmal.com>
2023-09-11 23:24:36 +0530

    Rotate left by one

--- Arrays/RotateLeftBy1.cpp
+++ Arrays/RotateLeftBy1.cpp
@@ -2,7 +2,26 @@
 #include<math.h>
 using namespace std;
 
+void leftRotate(int arr[], int n)
+{
+    int temp = arr[0];
+
+    for(int i=1; i<n; i++)
+    {
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main()
 {
+    int arr[] = {0,1,2,3,4,5,6,7,8,9};
 
+    leftRotate(arr,10);
 }
