c4584c46aa3bd9a5dea6086438c5cfe4a58a8b13
Adil Khan <adiliitk2k16@gmal.com>
2023-09-14 10:49:49 +0530

    Leaders in array

--- Arrays/LeadersInarray.cpp
+++ Arrays/LeadersInarray.cpp
@@ -2,7 +2,25 @@
 #include<math.h>
 using namespace std;
 
+void leaders(int arr[], int n)
+{
+    int curlead=arr[n-1];
+    cout<<curlead<<" ";
+    for(int i=n-2; i>=0; i--)
+    {
+        if(arr[i]>curlead)
+        {
+            cout<<arr[i]<< " ";
+            curlead=arr[i];
+        }
+    }
+
+    cout<<endl;
+}
+
 int main()
 {
+    int arr[] = {7,10,4,10,6,5,2};
 
+    leaders(arr,7);
 }
