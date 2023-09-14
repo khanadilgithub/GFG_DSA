575559f1ff52d6d6430fa38d5d898d1470364956
Adil Khan <adiliitk2k16@gmal.com>
2023-09-14 10:59:47 +0530

    max diffrence

--- Arrays/MaxDiff.cpp
+++ Arrays/MaxDiff.cpp
@@ -2,7 +2,25 @@
 #include<math.h>
 using namespace std;
 
+int maxDifference(int arr[], int n)
+{
+    int minval=arr[0];
+
+    int maxdiff = arr[1]-arr[0];
+
+    for(int i=1; i<n; i++)
+    {
+        maxdiff = max(maxdiff, arr[i]-minval);
+
+        minval = min(minval, arr[i]);
+    }
+
+    return maxdiff;
+}
+
 int main()
 {
+    int arr[] = {2,3,10,6,4,8,1};
 
+    cout<<maxDifference(arr, 7)<<endl;
 }
