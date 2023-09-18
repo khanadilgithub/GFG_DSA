62a69aeef5048648aba08ab53d80e689c88e2915
Adil Khan <adiliitk2k16@gmal.com>
2023-09-18 23:04:22 +0530

    majority elm stub functions

--- Arrays/MajorityElement.cpp
+++ Arrays/MajorityElement.cpp
@@ -2,6 +2,19 @@
 #include<math.h>
 using namespace std;
 
+//Naive Method
+int majorityElem(int arr[], int n)
+{
+
+}
+
+//Moores Voting Alogorithm method
+
+int majorityElm(int arr[], int n)
+{
+
+} 
+
 int main()
 {
 
