ac72841dbfc3db4f98839c29335350c6dbf0d1ec
Adil Khan <adiliitk2k16@gmal.com>
2023-09-18 21:55:10 +0530

    Longest even odd

--- Arrays/LongestEvenOdd.cpp
+++ Arrays/LongestEvenOdd.cpp
@@ -2,7 +2,30 @@
 #include<math.h>
 using namespace std;
 
+int longestevenOdd(int arr[], int n)
+{
+    int res=1;
+    int curmx=1;
+
+    for(int i=1; i<n; i++)
+    {
+        if((arr[i]%2==0 && arr[i-1]%2!=0) || (arr[i]%2!=0 && arr[i-1]%2==0))
+        {
+            curmx++;
+            res=max(res,curmx);
+        }
+        else 
+        {
+            curmx=1;
+        }
+    }
+
+    return res;
+}
+
 int main()
 {
+    int arr[] = {5,10,20,6,3,8};
 
+    cout<<longestevenOdd(arr, 6)<<endl;
 }
