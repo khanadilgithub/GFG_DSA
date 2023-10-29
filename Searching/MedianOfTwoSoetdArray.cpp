8f826a8f83a5244c1839a8d1f9bb7328fbd054af
Adil Khan <adiliitk2k16@gmal.com>
2023-10-29 20:46:15 +0530

    Median need to fix type conversio

---
+++ Searching/MedianOfTwoSoetdArray.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+int FindMedian(int arr1[], int arr2[], int n1, int n2)
+{
+    int size=n1+n2;
+    int temp[size] = {0};
+
+    for(int i=0; i<size; i++)
+    {
+        if(i<n1)
+        {
+            temp[i]=arr1[i];
+        }
+        else
+        {
+            temp[i]=arr2[i-n1];
+        }
+    }
+    sort(temp, temp+size);
+
+    if(size%2==0)
+    {
+        return (double)((double)temp[size/2]+(double)temp[(size/2)-1])/2.0;
+    }
+    else
+    {
+        return temp[size/2];
+    }
+}
+
+int main()
+{
+    int arr1[] = {10,20,30,40,50};
+    int arr2[] = {5,15,25,35,45};
+
+    cout<<FindMedian(arr1, arr2, 5,5)<<endl;
+
+}
