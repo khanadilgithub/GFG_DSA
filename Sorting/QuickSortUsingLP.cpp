70b313af0020f7b1e6e2602d5812059bde430bf3
Adil Khan <adiliitk2k16@gmal.com>
2023-11-18 00:35:01 +0530

    Quick Sort Using LP

--- Sorting/QuickSortUsingLP.cpp
+++ Sorting/QuickSortUsingLP.cpp
@@ -2,7 +2,44 @@
 #include<math.h>
 using namespace std;
 
+int lomutoPartition(int arr[], int lw, int hi)
+{
+    int pvt=arr[hi];
+    int lp=lw-1;
+
+    for (int i=lw; i<=hi-1; i++)
+    {
+        if(arr[i]<pvt)
+        {
+            swap(arr[++lp], arr[i]);
+        }
+    }
+    swap(arr[++lp], arr[hi]);
+    
+    return lp;
+}
+
+void quickSortLP(int arr[], int lw, int hi)
+{
+    if(lw<hi)
+    {
+        int pt= lomutoPartition(arr, lw, hi);
+
+        quickSortLP(arr,lw, pt-1);
+
+        quickSortLP(arr, pt+1, hi);
+    }
+}
+
 int main()
 {
+    int arr[] = {10,80,30,90,40,50,70};
 
+    quickSortLP(arr, 0, 6);
+
+    for(int i=0; i<7; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
